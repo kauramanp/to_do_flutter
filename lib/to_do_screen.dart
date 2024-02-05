d2797157bb40f0d26cc3a15127705df2f27d964e
kauramanp <33446281+kauramanp@users.noreply.github.com>
2024-02-06 00:10:23 +0530

    Initial commit

---
+++ lib/to_do_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class ToDoScreen extends StatefulWidget {
+  const ToDoScreen({super.key});
+
+  @override
+  State<ToDoScreen> createState() => _ToDoScreenState();
+}
+
+class _ToDoScreenState extends State<ToDoScreen> {
+  var list = <String>["Testing"];
+
+  _showPopUpDialog({int position = -1}) {
+    var textFieldController = TextEditingController();
+    var _validate = false;
+    if (position > -1) {
+      textFieldController.text = list[position];
+    }
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (context, alertState) {
+            return AlertDialog(
+              title: Text(position > -1 ? "Update Item" : "Add Item"),
+              content: TextField(
+                controller: textFieldController,
+                decoration: InputDecoration(
+                    labelText: "Enter the value",
+                    errorText: _validate ? "Enter value" : null),
+              ),
+              actions: [
+                OutlinedButton(
+                    onPressed: () {
+                      if (textFieldController.text.isEmpty) {
+                        _validate = textFieldController.text.isEmpty;
+                        alertState(() {});
+                        return;
+                      } else if (position > -1) {
+                        list[position] = textFieldController.text.toString();
+                      } else {
+                        list.add(textFieldController.text.toString());
+                      }
+                      setState(() {});
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                        position > -1 ? "Update List Item" : "Add In List"))
+              ],
+            );
+          });
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Welcome to ToDo App")),
+      body: Column(children: [
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+                onTap: () {
+                  _showPopUpDialog(position: index);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    list[index],
+                    style: TextStyle(fontSize: 35),
+                  ),
+                ));
+          },
+        )
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showPopUpDialog();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
